eb10bee5eb54a5d9c76a24a2b17d20f1e936ed48
Caiolis <kaiogesteira@gmail.com>
2023-08-25 01:38:23 -0300

    Deploy configs

--- src/app.js
+++ src/app.js
@@ -11,5 +11,5 @@ app.use(express.json());
 app.use(router);
 
 
-const PORT = process.env.port || 5000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
